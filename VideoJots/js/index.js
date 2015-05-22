2d1ef9c65d682b581ea45d55bc55838aeda40626
mcaravind <blogsooner@gmail.com>
2015-05-21 22:46:33 -0700

    Pause and resume using keyboard

--- VideoJots/js/index.js
+++ VideoJots/js/index.js
@@ -1,29 +1,65 @@
-﻿function runScript(e) {
+﻿$(function() {
+    window.tagArray = [];
+});
+
+function runScript(e) {
     if (e.keyCode === 13) {
         var tb = document.getElementById("tbNotes");
         //process
         var pnl = document.getElementById("pnlNotes");
         //pnl.value += tb.value;
         var text = tb.value;
-        var posChange = false;
-        if (text.charAt(0) === '/' && text.charAt(text.length - 1) === '/') {
-            //rewind if - number
-            //forward if + number
-            var inside = text.substring(1, text.length - 1);
-            var rewind = TryParseInt(inside, null);
-            if (rewind) {
-                //var ytplayer = document.getElementById('player');
-                //ytplayer.seekTo(ytplayer.getCurrentTime() + rewind);
-                player.seekTo(player.getCurrentTime() + rewind);
-                posChange = true;
+        var doNotDisplay = false;
+        if (text === '//') {
+            //pop last tag from array
+            window.tagArray.remove(window.tagArray.length - 1);
+            doNotDisplay = true;
+            displayTagArray();
+        } else if (text === '/p/') {
+            player.pauseVideo();
+            doNotDisplay = true;
+        }
+            else if (text === '/r/') {
+                player.playVideo();
+                doNotDisplay = true;
+        }
+        else {
+            if (text.charAt(0) === '/' && text.charAt(text.length - 1) === '/') {
+                //rewind if - number
+                //forward if + number
+                var inside = text.substring(1, text.length - 1);
+                var rewind = TryParseInt(inside, null);
+                if (rewind) {
+                    //var ytplayer = document.getElementById('player');
+                    //ytplayer.seekTo(ytplayer.getCurrentTime() + rewind);
+                    player.seekTo(player.getCurrentTime() + rewind);
+                    doNotDisplay = true;
+                } else {
+                    window.tagArray.push(inside);
+                    displayTagArray();
+                }
             }
         }
-        if (!posChange) {
+        if (!doNotDisplay) {
             $("#pnlNotes").append('<br/>' + tb.value);
         }
         tb.value = '';
         return false;
     }
+}
+
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function (from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
+
+function displayTagArray() {
+    $("#tagArray").html('');
+    $.each(window.tagArray, function(index, value) {
+        $("#tagArray").append(value+'<br/>');
+    });
 }
 
 function TryParseInt(str, defaultValue) {
